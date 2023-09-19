faf51bb3e54e05982107fe6adb35e087de6712da
vishesh <visheshtiwari@ggx.io>
2023-09-19 16:29:17 +0530

    added controller method to find a employee by id

--- src/main/java/com/example/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/example/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.example.cruddemo.rest;
 import com.example.cruddemo.entity.Employee;
 import com.example.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,16 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Could not find employee with id: " + employeeId);
+        }
+
+        return theEmployee;
     }
 }
