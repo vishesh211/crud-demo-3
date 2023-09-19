1e4a12085d34e08b574e337967405a0f966d3795
vishesh <visheshtiwari@ggx.io>
2023-09-19 16:08:14 +0530

    added crud operations in the service layer

--- src/main/java/com/example/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/cruddemo/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.cruddemo.dao.EmployeeDAO;
 import com.example.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,5 +20,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDao.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeDao.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee employee) {
+        return employeeDao.save(employee);
+    }
+
+
+    @Override
+    public void deleteById(int id) {
+        employeeDao.deleteById(id);
     }
 }
