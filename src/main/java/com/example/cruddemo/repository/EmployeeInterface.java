027b45ac9d352bf482e99a8b163cf89efc1b5c7e
vishesh <visheshtiwari@ggx.io>
2023-09-19 19:02:24 +0530

    added spring data rest configs and various other properties

--- src/main/java/com/example/cruddemo/repository/EmployeeInterface.java
+++ src/main/java/com/example/cruddemo/repository/EmployeeInterface.java
@@ -2,8 +2,9 @@ package com.example.cruddemo.repository;
 
 import com.example.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+//@RepositoryRestResource(path = "members")
 public interface EmployeeInterface extends JpaRepository<Employee, Integer> {
 }
